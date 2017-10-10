a74453f91cb04eed90b12f2b42fabd37f38cb1dd
Philip Jander <info@jander.it>
2017-10-10 06:44:10 +0200

    Cleand sandbox

--- Shared/VersionManifest.fs
+++ Shared/VersionManifest.fs
@@ -15,6 +15,9 @@ module Manifests =
     | FolderExpected of string
     | FileExpected of string * Resource
     | ExactFileExpected of string * Hash * Resource
+    //| ConfigFileExists of string * string
+    //| ProcessRuns of string * string * bool // (override command line)
+    //| AutoStartLink of string * string * string
 
   type VersionManifest =
     { Expectations : InstallerExpectations list }
